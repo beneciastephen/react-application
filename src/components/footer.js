d3d157cac668bd3ecb4b24d526a115b977b906d0
beneciastephen <benecia.stephen@gmail.com>
2026-03-05 22:10:19 -0500

    Updates

--- src/components/footer.js
+++
@@ -1,8 +0,0 @@
-// src/components/Footer.js
-export default function Footer() {
-  return (
-    <footer style={{ textAlign: "center", background: "#2a9d8f", color: "white", padding: "15px" }}>
-      <h3>Moral: Slow and steady wins the race.</h3>
-    </footer>
-  );
-}
--- src/components/header.js
+++
@@ -1,8 +0,0 @@
-// src/components/header.js
-export default function Header() {
-  return (
-    <header style={{ textAlign: "center", background: "#f4a261", padding: "20px" }}>
-      <h1>The Tortoise and the Hare</h1>
-    </header>
-  );
-}
